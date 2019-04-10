8f5dd4cd60e56012191f2012997c2cf09811b863
choper1996 <m.d.pakhomov@yandex.ru>
2019-04-10 08:37:57 +0200

    Bears - clean code for release
    js - es + camle case; html - unusing information

--- projects/bears/js/index.js
+++ projects/bears/js/index.js
@@ -1,17 +1,25 @@
 jQuery(document).ready(function($) {
 
 	// anchors
-	$('.nav_links, #main a').bind("click", function(e){
-	    var anchor = $(this);
-	    $('html, body').stop().animate({
+    const anchorLinks = $('.nav_links, #main a'),
+        window = $('html, body');
+
+    anchorLinks.bind("click", function(e){
+	    let anchor = $(this);
+
+        window.stop().animate({
 	        scrollTop: $(anchor.attr('href')).offset().top
 	    }, 1000);
+
 	    e.preventDefault();
 	});
 
 	// navigation
-	$('#burger').click(function() {
-		$('#header_txt_wrap').slideToggle(300);
+    const burger = $('#burger'),
+        headerText = $('#header_txt_wrap');
+
+    burger.click(function() {
+        headerText.slideToggle(300);
 	});
 
 	// popup
@@ -19,34 +27,43 @@ jQuery(document).ready(function($) {
 	    type: 'inline'
 	});
 
+	// popup close windows
 	$('.close_area').click(function() {
 		$('.mfp-close').click();
 	});
 
 	// choicee color
-	$('.bear_color').click(function() {
+    const bearColor = $('.bear_color'),
+        bearImg = $('.bear_wrap > img');
+
+    bearColor.click(function() {
 		$(this).siblings().removeClass('active_color');
 		$(this).addClass('active_color');
-		$('.bear_wrap > img').removeClass('active');
+        bearImg.removeClass('active');
 
-		var colorId = $(this).attr('id');
+		let colorId = $(this).attr('id');
 
 		$('#bear_' + colorId).addClass('active');
 	});
 
 	// FAQ toggle
-	$('#faq section').click(function(e) {
+	const faqSection = $('#faq section'),
+        faqAnswer = $('.answer');
+
+    faqSection.click(function(e) {
 		if ( $(this).attr('class') === 'faq_hover') {
-			$(this).find('.answer').slideToggle(300);
+			$(this).find(faqAnswer).slideToggle(300);
 			$(this).removeClass();
 		} else {
-			$(this).find('.answer').slideToggle(300);
+			$(this).find(faqAnswer).slideToggle(300);
 			$(this).addClass('faq_hover');
 		}
 	});
 
-	// instagramm slider
-	$('.inst_slider').slick({
+	// instagram slider
+	const instagramSlider = $('.inst_slider');
+
+    instagramSlider.slick({
 	    autoplay: true,
 	    dots: true,
 	    infinite: true,
@@ -72,5 +89,6 @@ jQuery(document).ready(function($) {
 	        }
 	      ]
 	});
+    //end ready
 });
 
--- projects/bears/js/send_forms.js
+++ projects/bears/js/send_forms.js
@@ -1,24 +1,31 @@
 $(document).ready(function () {
 
+    const buyBtn        = $('#buy_btn'),
+        backcallBtn     = $('#call_back_btn'),
+        withoutInfBtn    = $('#without_inf_btn'),
+        buyStatus       = $('#buy_form .status'),
+        backcallStatus  = $('#back_call .status'),
+        withoutInfStatus = $('#form_without_info .status');
+
     // CALLBACK
-    var send_subs_callback = false,
-        send_subs_buy = false,
-        send_subs_wi = false;
+    let sendSubsCallback = false,
+        sendSubsBuy = false,
+        sendSubsWi = false;
 
-    $('#call_back_btn').click(function () {
+    backcallBtn.click(function () {
 
-        var name = $('#back_call input[name="name"]').val(),
+        const name = $('#back_call input[name="name"]').val(),
             tel = $('#back_call input[name="tel"]').val(),
             title = 'Обратный звонок';
 
-        if (send_subs_callback) {return false}
+        if (sendSubsCallback) {return false}
 
         if (name === ""){
-            $('#back_call .status').html('Пожалуйста, введите имя').css('color', 'red');
+            backcallStatus.html('Пожалуйста, введите имя').css('color', 'red');
             return false;
         }
         if (tel === ""){
-            $('#back_call .status').html('Пожалуйста, введите ваш номер телефона').css('color', 'red');
+            backcallStatus.html('Пожалуйста, введите ваш номер телефона').css('color', 'red');
             return false;
         }
 
@@ -35,64 +42,62 @@ $(document).ready(function () {
                 'tel'   : tel
             },
             success: function () {
-                send_subs_callback = true;
-                $('#back_call .status').html('Ваша заявка принята').css('color', 'green');
+                sendSubsCallback = true;
+                backcallStatus.html('Ваша заявка принята').css('color', 'green');
             },
-            error: function (object) {
-                $('#back_call .status').html('Ошибка, попробуйте позже').css('color', 'red');
+            error: function () {
+                backcallStatus.html('Ошибка, попробуйте позже').css('color', 'red');
             }
         });
         return false;
 
     });
 
-    $('#back_call input').click(function() {
-        $('#back_call .status').html('').css('color', '#4b4b4b');
+    buyStatus .click(function() {
+        backcallStatus.html('').css('color', '#4b4b4b');
     });
 
 
     // BUY
-    $('#buy_btn').click(function () {
+    buyBtn.click(function () {
 
-        var name = $('#buy_form input[name="name"]').val();
-        var tel = $('#buy_form input[name="tel"]').val();
-        var city = $('#buy_form input[name="city"]').val();
-        var color = $('.bear_wrap .color_list').attr('id');
-        var title = 'Покупка';
+        const name = $('#buy_form input[name="name"]').val(),
+            tel    = $('#buy_form input[name="tel"]').val(),
+            city   = $('#buy_form input[name="city"]').val(),
+            color  = $('.bear_wrap .color_list').attr('id');
 
-        if (send_subs_buy) {return false;}
+
+        if (sendSubsBuy) {return false;}
 
         if (name === ""){
-            $('#buy_form .status').html('Пожалуйста, введите имя').css('color', 'red');
+            buyStatus.html('Пожалуйста, введите имя').css('color', 'red');
             return false;
         }
         if (tel === ""){
-            $('#buy_form .status').html('Пожалуйста, введите ваш номер телефона').css('color', 'red');
+            buyStatus.html('Пожалуйста, введите ваш номер телефона').css('color', 'red');
             return false;
         }
 
         $.ajax({
             type: "POST",
-            // url: "https://services.thefund.io/",
+            url: "",
             dataType: 'text',
             header: {
-                // 'Access-Control-Allow-Origin': 'https://services.thefund.io/'
+                // 'Access-Control-Allow-Origin': ''
             },
             data: {
-                'title' : title,
+                'title' : 'Покупка',
                 'name'  : name,
                 'tel'   : tel,
                 'city'  : city,
                 'color' : color
             },
             success: function () {
-                send_subs_buy = true;
-                $('#buy_form .status').html('Ваша заявка принята');
-                $('#buy_form .status').css('color', 'green');
+                sendSubsBuy = true;
+                buyStatus.html('Ваша заявка принята').css('color', 'green');
             },
-            error: function (object) {
-                $('#buy_form .status').html('Ошибка, попробуйте позже');
-                $('#buy_form .status').css('color', 'red');
+            error: function () {
+                buyStatus.html('Ошибка, попробуйте позже').css('color', 'red');
             }
         });
         return false;
@@ -100,46 +105,44 @@ $(document).ready(function () {
     });
 
     $('#buy_form input').click(function() {
-        $('#buy_form .status').html('');
-        $('#buy_form .status').css('color', '#4b4b4b');
+        buyStatus.html('').css('color', '#4b4b4b');
     });
 
     // WITHOUT INFO
-    $('#without_inf_btn').click(function () {
+    withoutInfBtn.click(function () {
 
-        var name = $('#form_without_info input[name="name"]').val();
-        var tel = $('#form_without_info input[name="tel"]').val();
-        var title = 'Без информации';
+        const name = $('#form_without_info input[name="name"]').val(),
+            tel = $('#form_without_info input[name="tel"]').val();
 
-        if (send_subs_wi) {return false}
+        if (sendSubsWi) {return false}
 
         if (name === ""){
-            $('#form_without_info .status').html('Пожалуйста, введите имя');
+            withoutInfStatus.html('Пожалуйста, введите имя');
             return false;
         }
         if (tel === ""){
-            $('#form_without_info .status').html('Пожалуйста, введите ваш номер телефона');
+            withoutInfStatus.html('Пожалуйста, введите ваш номер телефона');
             return false;
         }
 
         $.ajax({
             type: "POST",
-            // url: "https://services.thefund.io/",
+            url: "",
             dataType: 'text',
             header: {
-                // 'Access-Control-Allow-Origin': 'https://services.thefund.io/'
+                'Access-Control-Allow-Origin': ''
             },
             data: {
-                'title' : title,
+                'title' : 'Без информации',
                 'name'  : name,
                 'tel'   : tel
             },
             success: function () {
-                send_subs_wi = true;
-                $('#form_without_info .status').html('Ваша заявка принята');
+                sendSubsWi = true;
+                withoutInfStatus.html('Ваша заявка принята');
             },
-            error: function (object) {
-                $('#form_without_info .status').html('Ошибка, попробуйте позже');
+            error: function () {
+                withoutInfStatus.html('Ошибка, попробуйте позже');
             }
         });
         return false;
@@ -147,6 +150,6 @@ $(document).ready(function () {
     });
 
     $('#form_without_info input').click(function() {
-        $('#form_without_info .status').html('');
+        withoutInfStatus.html('');
     });
 });
