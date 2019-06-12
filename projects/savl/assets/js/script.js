5c162f663265150041db256fa9d27558af2b7113
choper1996 <m.d.pakhomov@yandex.ru>
2019-06-12 20:06:10 +0200

    update

--- projects/savl/assets/js/script.js
+++ projects/savl/assets/js/script.js
@@ -38,5 +38,125 @@ document.addEventListener("DOMContentLoaded", function() {
 
         mainLiseItems[nextSlide].classList.add('active');
     });
+
+    // abilities
+
+    let registrationSlider = document.querySelector('.registration__slider');
+
+    $(registrationSlider).slick({
+        infinite:      true,
+        dots:          false,
+        pauseOnHover:  false,
+        draggable:     true,
+        autoplaySpeed: 3000,
+        speed:          1000,
+        autoplay:       true,
+        arrows:        false,
+        slidesToShow:      1,
+        slidesToScroll:    1,
+        vertical:       true
+    });
+
+    let registrationListItem = document.querySelectorAll('.registration-steps-list__item');
+
+    $(registrationListItem).click(function() {
+        $(registrationSlider).slick('slickGoTo', +$(this).attr('id'));
+    });
+
+    $(registrationSlider).on('beforeChange', function(event, slick, currentSlide, nextSlide){
+        registrationListItem.forEach((item)=> {
+            item.classList.remove('active')
+        });
+
+        registrationListItem[nextSlide].classList.add('active');
+    });
+
+    // navigation
+    // STICK NAVIGATION AND HOVER LINKS
+     let navLinks = document.querySelectorAll('.nav-list-item');
+
+    window.addEventListener('scroll', ()=> {
+
+        let mainY       = document.querySelector('#main').getBoundingClientRect().bottom,
+            abilities  = document.querySelector('#abilities').getBoundingClientRect().top,
+            registration = document.querySelector('#registration').getBoundingClientRect().top;
+
+
+        if (mainY > 0) {
+            navLinks.forEach((item)=>{
+                item.classList.remove('active');
+            });
+
+            navLinks[0].classList.add('active');
+        }
+
+        if (abilities < 100 && abilities > -100) {
+            navLinks.forEach((item)=>{
+                item.classList.remove('active');
+            });
+
+            navLinks[1].classList.add('active');
+        }
+
+        if (registration < 100 && registration > -100) {
+            navLinks.forEach((item)=>{
+                item.classList.remove('active');
+            });
+
+            navLinks[2].classList.add('active');
+        }
+    });
+
+    // ANCHOR LINKS
+    let anchorToMain    = document.querySelector('.header__anchor-to-main'),
+        main            = document.querySelector('.main'),
+        abilities       = document.querySelector('.abilities'),
+        downButton      = document.querySelector('.main__down-button'),
+        downButtonMobile = document.querySelector('.main__down-button-mobile'),
+        navigationLinks = document.querySelectorAll('.nav-list-item a');
+
+    downButton.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        abilities.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+
+    downButtonMobile.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        abilities.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+
+    anchorToMain.addEventListener('click', (e)=>{
+        e.preventDefault();
+
+        main.scrollIntoView({
+            behavior: 'smooth',
+            block: 'start'
+        });
+    });
+
+
+    navigationLinks.forEach((item)=>{
+        item.addEventListener('click', (e)=>{
+            e.preventDefault();
+
+            let selectedLink = e.target,
+                href = selectedLink.getAttribute('href'),
+                el   = document.querySelector(`${href}`);
+
+            el.scrollIntoView({
+                behavior: 'smooth',
+                block: 'start'
+            });
+        })
+    });
+
 });
 
